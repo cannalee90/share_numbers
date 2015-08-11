1f175f2b8a5945949b6b46e94f9ac44949bb710e
astyfx <astyfx@gmail.com>
2015-08-12 00:55:45 +0900

    rollback keys

--- config/initializers/bitly.rb
+++ config/initializers/bitly.rb
@@ -2,5 +2,5 @@ Bitly.use_api_version_3
 
 Bitly.configure do |config|
   config.api_version = 3
-  config.api_key = "###############"
+  config.api_key = "e0c91ca53d3e97a379fa96e0d4e7f06f818d1b1f"
 end
--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,5 +1,5 @@
 Recaptcha.configure do |config|
-  config.public_key  = '########'
-  config.private_key = '########'
+  config.public_key  = '6LeieAoTAAAAABWkTDnPmZ9CLybHWIl99PXEhpxX'
+  config.private_key = '6LeieAoTAAAAAOaEHIqxUzzWo4n5ThJ-x84zxmBO'
   config.use_ssl_by_default = :true
   end

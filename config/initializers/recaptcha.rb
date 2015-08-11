bbe1acdadcb0bd54b6bee4226b26d5f564ac5b98
Ubuntu <ubuntu@ip-172-31-1-169.ap-northeast-1.compute.internal>
2015-08-11 16:04:42 +0000

    Fix recaptcha

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,5 +1,5 @@
 Recaptcha.configure do |config|
-  config.public_key  = '6LeieAoTAAAAABWkTDnPmZ9CLybHWIl99PXEhpxX'
-  config.private_key = '6LeieAoTAAAAAOaEHIqxUzzWo4n5ThJ-x84zxmBO'
+  config.public_key  = '6Lf4HAsTAAAAAOFXelTXI7gb0AP6r5a5ZBcPuMo1'
+  config.private_key = '6Lf4HAsTAAAAALcKkMOhoW-qVSfGme-HNZB-0RSX'
   config.use_ssl_by_default = :true
   end
